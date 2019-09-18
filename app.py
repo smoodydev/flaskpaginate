a170325e79a37c64ddb2ea38981e68ed85b6b2e6
Stephen Moody <darilligames@gmail.com>
2019-09-18 08:15:53 +0000

    Working on pagination with flask

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+from flask import Flask, render_template
+from flask_paginate import Pagination, get_page_args
+from bson.objectid import ObjectId
+
+
+app = Flask(__name__)
+app.template_folder = ''
+
+app.config["MONGO_DBNAME"] = 'Cluster0'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb://localhost')
+
+print(app.config["MONGO_URI"])
+
+users = list(range(100))
+
+
+def get_users(offset=0, per_page=10):
+    return users[offset: offset + per_page]
+
+
+@app.route('/')
+def index():
+    page, per_page, offset = get_page_args(page_parameter='page',
+                                           per_page_parameter='per_page')
+    total = len(users)
+    pagination_users = get_users(offset=offset, per_page=per_page)
+    pagination = Pagination(page=page, per_page=per_page, total=total,
+                            css_framework='bootstrap4')
+    return render_template('index.html',
+                           users=pagination_users,
+                           page=page,
+                           per_page=per_page,
+                           pagination=pagination,
+                           )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
