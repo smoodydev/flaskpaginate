04244df38ac52d0ecef782f8259dd4673bfd61ef
Stephen Moody <darilligames@gmail.com>
2019-09-19 11:59:26 +0000

    Had some Fun

--- app.py
+++ app.py
@@ -7,30 +7,17 @@ from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'testerMongo'
-
-##app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
-
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
 app.templates = ""
 
-users = []
 
-for x in range(1,101):
-    string = ""
-    if x%3==0:
-        string = string+"Fizz"
-    elif x%5==0:
-        string = string+"Buzz"
-    else:
-        string = str(x)
-    users.append({"id" : x, "name": str(x)+" is my name",  "fb": string})
+itemtotal = mongo.db.mongoTestingDataBase.find().count()
 
 
-def get_users(offset=0, per_page=10):
-    return users[offset: offset + per_page]
+
 
 def get_tests(offset=0, per_page=10):
     thetests = mongo.db.mongoTestingDataBase.find()
@@ -40,18 +27,7 @@ def get_tests(offset=0, per_page=10):
 
 @app.route('/')
 def index():
-    page, per_page, offset = get_page_args(page_parameter='page',
-                                           per_page_parameter='per_page')
-    total = len(users)
-    pagination_users = get_users(offset=offset, per_page=per_page)
-    pagination = Pagination(page=page, per_page=per_page, total=total,
-                            css_framework='bootstrap4')
-    return render_template('index.html',
-                           users=pagination_users,
-                           page=page,
-                           per_page=per_page,
-                           pagination=pagination,
-                           )
+    return render_template('index.html')
 
 @app.route('/add')
 def addTest():
@@ -68,7 +44,7 @@ def insert_test():
 def showTests():
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
-    total = mongo.db.mongoTestingDataBase.count()
+    total = itemtotal
     paginatedTests = get_tests(offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
