bfa82bf1bf3e88b660e5dbf5290ec46211f71414
Stephen Moody <darilligames@gmail.com>
2019-11-07 17:13:52 +0000

    added ports

--- app.py
+++ app.py
@@ -59,4 +59,6 @@ def showTests():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
