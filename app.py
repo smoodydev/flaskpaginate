b72d7783756e36f43dc192b8344ef3a8bac4a4ca
Stephen Moody <darilligames@gmail.com>
2019-11-07 17:19:51 +0000

    added some stuff

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def showTests():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
